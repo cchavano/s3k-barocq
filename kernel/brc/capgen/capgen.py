4113ffff19903bae7f35f77556d496990995e5e5
Henrik Akira Karlsson <hakarlsson@proton.me>
2025-04-30 10:08:06 +0200

    Adds barocq code.

---
+++ kernel/brc/capgen/capgen.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+import argparse
+import re
+import yaml
+
+
+def binpack(bin_capacities, items):
+    """
+    Solves the binpacking problem optimally
+    """
+    if not items:
+        return {i: [] for i in bin_capacities.keys()}
+    name, size = items[0]
+    for i, capacity in bin_capacities.items():
+        if size > capacity:
+            continue
+        new_bins = bin_capacities.copy()
+        new_bins[i] -= size
+        packing = binpack(new_bins, items[1:])
+        if packing is not None:
+            packing[i].append(name)
+            return packing
+    return None
+
+
+class Capability:
+    def __init__(self, name, fields):
+        self._name = name
+        self._fields = fields.keys()
+        self._sizes = fields.copy()
+        items = list(fields.items())
+        self._alloc = binpack({'word0': 60}, items)
+        if not self._alloc:
+            raise Exception(f"No allocation for {name} capability")
+        self._offsets = {}
+        for word, word_fields in self._alloc.items():
+            offset = 4
+            for field in word_fields[::-1]:
+                self._offsets[field] = offset
+                offset += self._sizes[field]
+
+    def type(self):
+        return f"CAPTY_{self._name.upper()}"
+
+    def name(self):
+        return self._name
+
+    def size(self, field):
+        return self._sizes[field]
+
+    def offset(self, field):
+        return self._offsets[field]
+
+    def word(self, field):
+        for word, fields in self._alloc.items():
+            if field in fields:
+                return word
+        return None
+
+    def fields(self, word=None):
+        if word:
+            return self._alloc[word]
+        return self._fields
+
+    def constr_fun(self):
+        output = []
+        parameters = [f"{name}: u64" for name in self.fields() if name != "pad"]
+        output.append(f"def cap_mk_{self.name()}({', '.join(parameters)}) : u64 =")
+        builder = f"  {self.type()}"
+        for field in self.fields('word0'):
+            if field == "pad":
+                continue
+            offset = self.offset(field)
+            builder += f" | {field} << {offset}"
+        builder += ";;"
+        output.append(builder)
+        return "\n".join(output)
+
+    def getter_fun(self, field):
+        name = self.name()
+        word = self.word(field)
+        offset = self.offset(field)
+        mask = (1 << self.size(field)) - 1
+        output = []
+        output.append(f"def cap_{name}_get_{field}(cap: cap_t) : u64 =")
+        if offset:
+            output.append(f"  (cap >> {offset}) & {hex(mask)}UL;;")
+        else:
+            output.append(f"  (cap & {hex(mask)}UL);;")
+        return "\n".join(output)
+
+    def setter_fun(self, field):
+        name = self.name()
+        word = self.word(field)
+        offset = self.offset(field)
+        mask = (1 << self.size(field)) - 1
+        output = []
+        output.append(f"def cap_{name}_set_{field}(cap: cap_t, v: u64) : u64 =")
+        if offset:
+            mask = mask << offset
+            output.append(f"  (cap & ~{hex(mask)}UL) | (v << {offset});;")
+        else:
+            output.append(f"  (cap & ~{hex(mask)}UL) | v;;")
+        return "\n".join(output)
+
+
+# Open the file and load the file
+def main(capabilities):
+    output = []
+    for i, cap in enumerate(capabilities):
+        output.append(f"def CAPTY_{cap['name'].upper()} : u64 = {i}UL;;")
+    output.append(f"def CAPTY_COUNT : u64 = {len(capabilities)}UL;;")
+    output.append(f"def cap_get_type(cap: cap_t) : u64 = (cap & 0xfUL);;")
+    for cap in capabilities:
+        capability = Capability(**cap)
+        output.append(capability.constr_fun())
+        for field in capability.fields():
+            if field == "pad":
+                continue
+            output.append(capability.getter_fun(field))
+        for field in capability.fields():
+            if field == "pad":
+                continue
+            output.append(capability.setter_fun(field))
+
+    output = "\n".join(output)
+    output = output.replace("\t", " "*8)
+    return output
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+            prog="gen_cap",
+            description="Generate capability header "
+                        "file from yaml description")
+    parser.add_argument("yaml", type=argparse.FileType('r'))
+    parser.add_argument("output", type=argparse.FileType('w'))
+    args = parser.parse_args()
+    data = yaml.safe_load(args.yaml)
+    print(main(**data), file=args.output)
