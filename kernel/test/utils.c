13693f49c3885411076a7e1ce1712b0fcd3d7d13
Henrik Karlsson <hakarlsson@proton.me>
2025-06-12 12:15:01 +0200

    Fixes ipc_send_recv

--- kernel/test/utils.c
+++ kernel/test/utils.c
@@ -92,11 +92,6 @@ uint64_t rtc_time_get(void)
 	return __mtime[0]++;
 }
 
-u64 Machine_time_read(struct Machine_state *mc)
-{
-	return rtc_time_get();
-}
-
 void rtc_time_set(uint64_t time)
 {
 	__mtime[0] = time;
@@ -110,4 +105,19 @@ uint64_t rtc_timeout_get(uint64_t hart)
 void rtc_timeout_set(uint64_t hart, uint64_t time)
 {
 	__mtimecmp[hart] = time;
+}
+
+u64 Machine_time_read(struct Machine_state *mc)
+{
+	return rtc_time_get();
+}
+
+u64 Machine_timeout_read(struct Machine_state *mc, u64 hart)
+{
+	return rtc_timeout_get(hart);
+}
+
+u64 Machine_csrr_mhartid(struct Machine_state *mc)
+{
+	return csrr_mhartid();
 }
